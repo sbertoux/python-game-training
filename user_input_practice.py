61965f1710affa7e0973a1510f3b75065d2a09d2
sbertoux <sbertoux73@gmail.com>
2025-06-22 16:10:18 -0400

    Module 3

---
+++ user_input_practice.py
@@ -0,0 +1,5 @@
+fav_colr = input("what is your favorite color?")
+lucky_num_str = input("what is your lucky number?")
+lucky_num = int(lucky_num_str)
+message = f"Your favorite color is {fav_colr} and your lucky number is {lucky_num}"
+print (message)
---
+++ variables_and_types.py
@@ -0,0 +1,19 @@
+qty = 5 
+cost = 12.50
+shipping = 2
+total_cost = qty * cost + shipping
+print (f'total cost is $', total_cost)
+
+
+
+noun = "dog" 
+adjective = "blind" 
+message =  f"the {adjective} {noun} runs fast"
+print (message)
+
+
+
+is_sunny = True 
+is_weekend = False 
+print (is_sunny and is_weekend)
+print (is_sunny or is_weekend)
